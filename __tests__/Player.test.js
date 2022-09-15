ca86a1863ab5c6bb606f9baaa81765cfc814dd9c
tracybrtn <tracynburton@gmail.com>
2022-09-15 12:45:58 -0500

    Player object basic info added' git push origin feature/player

---
+++ __tests__/Player.test.js
@@ -0,0 +1,19 @@
+const Player = require('../lib/Player.js');
+const Potion = require('../lib/Potion.js');
+
+jest.mock('../lib/Potion.js');
+
+console.log(new Potion());
+
+
+
+it('creates a player object', () => {
+    const player = new Player('Dave');
+
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)])
+    );
+})
