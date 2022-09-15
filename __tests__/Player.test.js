757477ee754d5a86e9e1a4f10c98742982a8c94f
tracybrtn <tracynburton@gmail.com>
2022-09-15 13:05:12 -0500

    all tests passed for player object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -17,3 +17,22 @@ it('creates a player object', () => {
     expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)])
     );
 })
+
+it("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+})
+
+it('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+})
